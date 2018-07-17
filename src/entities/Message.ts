c12f4e667ecab63e083dcb12dd5823e542fd0e8a
Bumkyu <bombkyu@gmail.com>
2018-07-17 11:19:37 +0900

    Create Chat, Message GraphQL & entity

---
+++ src/entities/Message.ts
@@ -0,0 +1,32 @@
+import {
+    BaseEntity,
+    Column,
+    CreateDateColumn,
+    Entity,
+    ManyToOne,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+   
+} from "typeorm";
+
+import Chat from "./Chat";
+import User from "./User";
+
+@Entity()
+class Message extends BaseEntity {
+    @PrimaryGeneratedColumn() id: number;
+
+    @ManyToOne(type => Chat, chat=>chat.messages)
+    chat: Chat
+
+    @ManyToOne(type=>User, user=>user.messages)
+    user: User
+
+    @Column({ type: 'text' })
+    text: string;
+
+    @CreateDateColumn() createdAt: string;
+    @UpdateDateColumn() updatedAt: string;
+
+}
+export default Message;
