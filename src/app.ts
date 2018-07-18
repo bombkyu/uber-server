1d1466abaa75516e4c214ae586c82470eb8e51e9
Bumkyu <bombkyu@gmail.com>
2018-07-18 13:24:30 +0900

    Create GetMyProfile Resolver

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,22 @@ import helmet from "helmet";
 import logger from "morgan";
 import schema from "./schema";
 import decodeJWT from './utils/decodeJWT';
+import { NextFunction, Response } from '../node_modules/@types/express';
 
 class App {
     public app:GraphQLServer;
     constructor() {
         // GraphQLSever를 생성하려면
         // Schema, resolver가 있어야한다.
+        // If we define Context here then you can access request from all the resolvers.
+        // Context is something that goes to all the resolvers.
         this.app = new GraphQLServer({
-            schema
+            schema,
+            context: req => {
+                return {
+                    req:req.request
+                }
+            }
         });
         this.middlewares();
     }
@@ -26,14 +34,22 @@ class App {
 
     // This is JWT Middleware
     // Decode Token and check if it is the right user.
-    private jwt = async (req, res, next) : Promise<void>=> {
+    private jwt = async (req, res:Response, next:NextFunction) : Promise<void>=> {
         // The token sent from user is included in request header
         const token = req.get("X-JWT");
         if(token) {
 
             // decodeJWT will return a user if the token has appropriate id.
-           const user = await decodeJWT(token);
-           console.log(user);
+            const user = await decodeJWT(token);
+
+            // Attach user to the request header, 
+            // so after we pass all the Middlewares 
+            // we can use User on the GraphQL Server
+            if(user) {
+                req.user = user;
+            } else {
+                req.user = undefined;
+            }
         }
         // if jwt middleware is finished, go and run next middleware
         next();
