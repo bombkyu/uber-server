49679841ad5b6aa21d7f43297d4792bd74f9cb48
Bumkyu <bombkyu@gmail.com>
2018-07-19 14:38:19 +0900

    Change UpdateMyProfile

--- src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
+++ src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
@@ -27,6 +27,17 @@ const resolvers: Resolvers = {
             // 1. What do you want to update
             // 2. How would you like to update
             try {
+                // Why do we need this part?
+                // Well, If we just call update() function, then from the User Entity 
+                // the @BeforeUpdate is not going to called,
+                // and this will make password unhashed.
+                // Then when does @BeforeUpdate, @BeforeInsert get triggered?
+                // They will be triggered when I change user object property and save it.
+                // That's why I set the user.password and user.save()
+                if(args.password !== null) {
+                    user.password = args.password;
+                    user.save();
+                }
                 await User.update({ id: user.id }, { ...notNull })
                 return {
                     ok:true,
